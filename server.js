11d54fac58a20008bf439657627867d9f5c56fc5
Hugo Erazo <hdavider@gmail.com>
2023-12-28 14:16:17 -0600

    Cambios con middleware

--- server.js
+++ server.js
@@ -1,39 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = 3000;
 const tareas = [];
 
+// Usuarios
+const usuarios = [
+    { id: 1, nombre: "hugo", contrasena: "contrasena1" },
+];
+
+// Middleware para analizar JSON
 app.use(bodyParser.json());
 
-
-function validateRequestBody(req, res, next) {
-    if (!req.body || Object.keys(req.body).length === 0) {
-        return res.status(400).json({ message: 'El cuerpo de la solicitud está vacío.' });
-    }
-    if (req.method === 'POST' && !req.body.descripcion) {
-        return res.status(400).json({ message: 'Falta información válida para crear la tarea.' });
-    }
-    if (req.method === 'PUT' && (req.body.indice === undefined || req.body.indice === null)) {
-        return res.status(400).json({ message: 'Falta información válida para editar la tarea.' });
-    }
-    next();
-}
-
-
-function validateHttpMethods(req, res, next) {
-    const validMethods = ['GET', 'POST', 'PUT', 'DELETE'];
-    if (!validMethods.includes(req.method)) {
-        return res.status(405).send('Método HTTP no permitido.');
-    }
-    next();
-}
-
-app.use(validateHttpMethods);
-
+// Ruta principal para servir index.html
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, 'index.html');
     fs.readFile(filePath, 'utf8', (err, data) => {
@@ -45,73 +29,98 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/tareas', (req, res) => {
-    const tareasConIndice = tareas.map((tarea, indice) => ({ ...tarea, indice: indice + 1 }));
-    res.json(tareasConIndice);
-});
+// Middleware para validar JWT
+function validarJWT(req, res, next) {
+    const token = req.headers['authorization'];
 
-app.post('/tareas', validateRequestBody, (req, res) => {
-    const nuevaTarea = req.body;
-    agregarTarea(nuevaTarea.descripcion);
-    res.status(201).json({ message: 'Tarea agregada correctamente.' });
-});
+    if (!token) {
+        return res.sendStatus(401);
+    }
 
-app.put('/tareas', validateRequestBody, (req, res) => {
-    const { indice } = req.body;
-    const tareaExistente = completarTarea(indice);
+    jwt.verify(token, process.env.JWT_SECRET, (err, usuario) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        req.usuario = usuario;
+        next();
+    });
+}
 
-    if (tareaExistente) {
-        res.json({ message: `Tarea #${indice} marcada como completada.` });
+// Middleware para validar el cuerpo de la solicitud, aplicado solo a ciertas rutas
+function validateRequestBody(req, res, next) {
+    if (req.method === 'POST' && !req.body.descripcion) {
+        return res.status(400).json({ message: 'Falta información válida para crear la tarea.' });
+    }
+    if (req.method === 'PUT' && (typeof req.body.estado !== 'boolean')) {
+        return res.status(400).json({ message: 'Falta información válida para actualizar la tarea.' });
+    }
+    next();
+}
+
+// Ruta de autenticación para crear JWT
+app.post('/login', (req, res) => {
+    const { nombre, contrasena } = req.body;
+    const usuario = usuarios.find(u => u.nombre === nombre && u.contrasena === contrasena);
+
+    if (usuario) {
+        const token = jwt.sign({ id: usuario.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ mensaje: "Autenticación exitosa", token });
     } else {
-        res.status(404).json({ message: 'La tarea no existe.' });
+        res.status(401).json({ mensaje: "Nombre de usuario o contraseña incorrectos" });
     }
 });
 
-app.delete('/tareas/:indice', (req, res) => {
-    const indice = parseInt(req.params.indice, 10);
-    const tareaEliminada = eliminarTarea(indice);
 
-    if (tareaEliminada) {
-        res.json({ message: `Tarea #${indice} eliminada.` });
-    } else {
-        res.status(404).json({ message: 'La tarea no existe.' });
+// Rutas para la gestión de tareas
+app.get('/tareas', validarJWT, (req, res) => {
+    const { estado } = req.query;
+    let tareasFiltradas = [...tareas];
+
+    if (estado !== undefined) {
+        const estadoBool = estado === 'completas';
+        tareasFiltradas = tareas.filter(tarea => tarea.estado === estadoBool);
     }
+
+    res.json(tareasFiltradas);
 });
 
-app.use((req, res) => {
-    res.status(404).send('404 Not Found');
-});
-
-function agregarTarea(descripcion) {
-    if (!descripcion || descripcion.trim() === '') {
-        descripcion = 'Tarea Generica';
-    }
-    const nuevaTarea = { descripcion, estado: false };
+app.post('/tareas', validarJWT, validateRequestBody, (req, res) => {
+    const nuevaTarea = { descripcion: req.body.descripcion, estado: false };
     tareas.push(nuevaTarea);
-}
+    res.status(201).json({ mensaje: 'Tarea agregada correctamente.' });
+});
 
-function completarTarea(indice) {
-    const tarea = tareas[indice - 1];
-    if (tarea) {
-        tarea.estado = true;
-        console.log(`Tarea #${indice} marcada como completada.`);
-        return true;
-    } else {
-        console.error('La tarea no existe.');
-        return false;
-    }
-}
+app.put('/tareas/:indice', validarJWT, validateRequestBody, (req, res) => {
+    const indice = parseInt(req.params.indice, 10) - 1;
 
-function eliminarTarea(indice) {
-    if (indice > 0 && indice <= tareas.length) {
-        tareas.splice(indice - 1, 1);
-        console.log(`Tarea #${indice} eliminada.`);
-        return true;
+    if (indice >= 0 && indice < tareas.length) {
+        tareas[indice].estado = req.body.estado;
+        res.json({ mensaje: `Tarea #${indice + 1} actualizada correctamente.` });
     } else {
-        console.error('La tarea no existe.');
-        return false;
+        res.status(404).json({ mensaje: 'La tarea no existe.' });
     }
-}
+});
+
+app.delete('/tareas/:indice', validarJWT, (req, res) => {
+    const indice = parseInt(req.params.indice, 10) - 1;
+
+    if (indice >= 0 && indice < tareas.length) {
+        tareas.splice(indice, 1);
+        res.json({ mensaje: `Tarea #${indice + 1} eliminada correctamente.` });
+    } else {
+        res.status(404).json({ mensaje: 'La tarea no existe.' });
+    }
+});
+
+app.get('/tareas/:indice', validarJWT, (req, res) => {
+    const indice = parseInt(req.params.indice, 10) - 1;
+
+    if (indice >= 0 && indice < tareas.length) {
+        res.json(tareas[indice]);
+    } else {
+        res.status(404).json({ mensaje: 'La tarea no existe.' });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
